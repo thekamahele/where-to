6d58a153dbc10eea939456bb49a9f5a4644c4724
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-11-09 13:41:41 -0800

    opened connection to mongodb

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
@@ -7,21 +6,20 @@ var methodOverride = require('method-override');
 var port  = process.env.PORT || 3000;
 var app = express();
 
-mongoose.connect("mongodb://localhost/where-to");
+require('./db/db-config')
 
-app.configure(function() {
-  app.use(express.static(__dirname + '/client'));
-  app.use('client/lib',  express.static(__dirname + '/client/lib'));
-  app.use(morgan('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use(bodyParser.text());
-  app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
-  app.use(methodOverride());
-})
+app.use(express.static(__dirname + '/client'));
+app.use('client/lib',  express.static(__dirname + '/client/lib'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
 // Routes
 // ------------------------------------------------------
-// require('./app/routes.js')(app);
+//require('./routes/routes.js')(app);
 
 // Listen
 // -------------------------------------------------------
