ca43f300a25bc0038bea4d804bfd5a67336e94eb
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-11-10 10:55:33 -0800

    able to create user

--- client/app/controllers/authController.js
+++ client/app/controllers/authController.js
@@ -1,6 +1,43 @@
-angular.module('whereTo.auth', [])
-  .controller('AuthController', [$scope, $firebase, function(){
-    var ref = new Firebase("https://where-to-next.firebaseio.com/");
+angular.module('whereTo.auth', ['firebase'])
+  .controller('AuthController', ['$scope', function($scope){
+    var fbRef = new Firebase("https://where-to-next.firebaseio.com");
 
+    var checkAuth = new FirebaseSimpleLogin(fbRef, function(err, user) {
+      if (err !== null) {
+          console.log("Error authenticating:", err);
+        } else if (user !== null) {
+          console.log("User is logged in:", user);
+        } else {
+          console.log("User is logged out");
+        }
+    });
+
+    $scope.login = function() {
+      var email = $scope.user.email;
+      var password = $scope.user.password;
+
+      checkAuth.login('password', {
+        email: email,
+        password: password
+      })
+    }
+
+    $scope.signup = function() {
+
+      var email = $scope.user.email;
+      var password = $scope.user.password;
+
+      checkAuth.createUser(email, password, function(error, user) {
+        if (error === null) {
+          console.log("User created successfully:", user);
+        } else {
+          console.log("Error creating user:", error);
+        }
+      });
+    }
+
+    $scope.logout = function() {
+      checkAuth.logout();
+    }
 
   }])
