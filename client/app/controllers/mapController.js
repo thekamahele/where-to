db147800fec8908744b11830ab6ca542901aeb33
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-11-09 21:58:38 -0800

    stylized map

--- client/app/controllers/mapController.js
+++ client/app/controllers/mapController.js
@@ -3,10 +3,62 @@ angular.module('whereTo.map', [])
   .controller('MapController', function($scope, MapService){
     $scope.location
 
+    var styles = [{
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [{
+            "hue": "#008285"
+        }, {
+            "saturation": 100
+        }, {
+            "lightness": -66
+        }, {
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [{
+            "hue": "#CAFCE4"
+        }, {
+            "saturation": 85
+        }, {
+            "lightness": 0
+        }, {
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "poi.park",
+        "elementType": "all",
+        "stylers": [{
+            "hue": "#61C273"
+        }, {
+            "saturation": 2
+        }, {
+            "lightness": -27
+        }, {
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [{
+            "hue": "#B0C4C7"
+        }, {
+            "saturation": -83
+        }, {
+            "lightness": 26
+        }, {
+            "visibility": "on"
+        }]
+    }]
+    
     var map = new google.maps.Map(document.getElementById('mapdisplay'), {
         zoom: 2,
         center: new google.maps.LatLng(0,0)
     });
+
+    map.setOptions({styles: styles});
 
 
     $scope.findLoc = function() {
@@ -23,7 +75,7 @@ angular.module('whereTo.map', [])
             position: results[0].geometry.location
         });
         //function to insert coordinates into database
-        
+
       } else {
         alert("Geocode was not successful for the following reason: " + status);
         }
