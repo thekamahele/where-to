b47e57b4211edd3418d24a3b1db1e88a35235167
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-11-11 12:13:52 -0800

    code cleanup

--- client/app/controllers/mapController.js
+++ client/app/controllers/mapController.js
@@ -1,36 +1,36 @@
 angular.module('whereTo.map', [])
 
-  .controller('MapController', function($scope, $state, $stateParams){
-    var fbRef = new Firebase("https://where-to-next.firebaseio.com");
+.controller('MapController', function($scope, $state, $stateParams) {
+    var fbRef = new Firebase("URL HERE");
 
     //check if user is authorized, if not redirect to login
     var authData = fbRef.getAuth();
 
     $scope.fetchMarkers = function() {
-      fbRef.child('users').child(authData.uid).once('value', function(snapshot){
-        var places = snapshot.val().whereToList;
-        console.log(places)
+      fbRef.child('users').child(authData.uid).once('value', function(snapshot) {
+          var places = snapshot.val().whereToList;
+          console.log(places)
 
-        for(var key in places) {
-          console.log(places[key])
-          $scope.pinMap(places[key]);
-        }
-
-        setTimeout(function() {
-          for(var i = 0; i < leftOut.length; i++) {
-            $scope.pinMap(leftOut[i]);
+          for (var key in places) {
+              console.log(places[key])
+              $scope.pinMap(places[key]);
           }
-        }, 30000)
 
-      }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
+          setTimeout(function() {
+              for (var i = 0; i < leftOut.length; i++) {
+                  $scope.pinMap(leftOut[i]);
+              }
+          }, 30000)
+
+      }, function(errorObject) {
+          console.log("The read failed: " + errorObject.code);
       });
     }
 
 
-//******************************************************//
-        //**************** MAP ******************//   
-//******************************************************// 
+    //******************************************************//
+    //**************** MAP ******************//   
+    //******************************************************// 
 
     var styles = [{
         "featureType": "water",
@@ -84,62 +84,69 @@ angular.module('whereTo.map', [])
 
     var map = new google.maps.Map(document.getElementById('mapdisplay'), {
         zoom: 2,
-        center: new google.maps.LatLng(0,0)
+        center: new google.maps.LatLng(0, 0)
     });
 
-    map.setOptions({styles: styles});
-//******************************************************//
-//******************************************************//   
-//******************************************************//    
+    map.setOptions({
+        styles: styles
+    });
+    //******************************************************//
+    //******************************************************//   
+    //******************************************************//    
+
     var leftOut = [];
     $scope.pinMap = function(location) {
       var geocoder = new google.maps.Geocoder();
-      
-      geocoder.geocode({address: location}, function(results, status) {
-        console.log('here',location, status, google.maps.GeocoderStatus.OK)
-      if (status == google.maps.GeocoderStatus.OK) {
-        
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location,
-            icon: './assets/airplane.png'
-        });
-      } else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-        leftOut.push(location);
-        console.log(leftOut)
-      }
-    });
-  }
+
+      geocoder.geocode({
+          address: location
+      }, function(results, status) {
+          console.log('here', location, status, google.maps.GeocoderStatus.OK)
+          if (status == google.maps.GeocoderStatus.OK) {
+
+              var marker = new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location,
+                  icon: './assets/airplane.png'
+              });
+          } else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+              leftOut.push(location);
+              console.log(leftOut)
+          }
+      });
+    }
 
     if (authData === null) {
-      $state.go('login')
+        $state.go('login')
     } else {
-      $scope.fetchMarkers();
+        $scope.fetchMarkers();
     }
-  
+
     $scope.location;
 
     $scope.findLoc = function() {
       $scope.location = $scope.map.location
-      //send to geocoder in mapservice
+          //send to geocoder in mapservice
       var geocoder = new google.maps.Geocoder();
 
-      geocoder.geocode({address: $scope.location}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location,
-            icon: './assets/airplane.png'
-        });
-        //function to insert coordinates into database
-        fbRef.child('users').child(authData.uid).child('whereToList').push($scope.location)
+      geocoder.geocode({
+          address: $scope.location
+      }, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
 
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-        }
+              var marker = new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location,
+                  icon: './assets/airplane.png'
+              });
+              //function to insert coordinates into database
+              fbRef.child('users').child(authData.uid).child('whereToList').push($scope.location)
+
+          } else {
+              alert("Geocode was not successful for the following reason: " + status);
+          }
       })
       $scope.map.location = ''
     }
 
-  });
+});
